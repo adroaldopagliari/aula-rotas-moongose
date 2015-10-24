b5f577c10a6852b785abba2b4de081e96a02555e
Adroaldo Pagliari <adroaldo@outlook.com>
2015-10-24 11:30:31 -0200

    Adicionado o controller

--- controller.js
+++ controller.js
@@ -1,8 +1,3 @@
- 
-var http = require('http');
-
-var Model = require('./model')
-
 var Controller = {
 	create: function(req, res) {
 	var dados = {
@@ -74,29 +69,4 @@ var Controller = {
 	}
 }
 
-http.createServer(function (req, res) {
-	console.log('url: ', req.url);
-	var url = req.url;
-	
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	switch(url) {
-		case '/create':
-			Controller.create(req, res);
-			break;
-		case '/retrieve':
-			Controller.retrieve(req, res);
-			break;
-		case '/update':
-			Controller.update(req, res);
-			break;
-		case '/delete':	
-			Controller.delete(req, res);
-			break;
-		default:
-			console.log("Nada a fazer");
-	}
-	
-}).listen(3000);
-
- 
-
+module.exports = Controller;
